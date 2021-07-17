9595c98864004bf2f4f0f76ef1a7b77e7da9fa1a
Francisco Ruiz <fruiz.rc@gmail.com>
2021-07-17 01:51:21 -0400

    add assetModuleFilename

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
 
   mode: "production",
